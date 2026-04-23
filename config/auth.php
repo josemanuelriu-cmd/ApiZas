3e30071a71814682efa5d9f5eba80c79eb1475b6
Riu <josemanuelriu@gmail.com>
2026-04-23 12:54:26 +0200

    Added basic routes and providers

--- config/auth.php
+++ config/auth.php
@@ -111,5 +111,10 @@ return [
     */
 
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
+    
+    'api' => [
+        'driver'   => 'passport',
+        'provider' => 'users',
+    ],
 
 ];
