ec4ab07330ab3115d5b53625f82e2277bc76964b
Riu <josemanuelriu@gmail.com>
2026-04-24 15:22:21 +0200

    added routes for new functions

--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,22 @@ Route::get('/', function () {
 Route::post('/v1/login', [UserController::class, 'login']);
 Route::post('/v1/register', [UserController::class, 'register']);
 Route::middleware('auth:api')->group(function () {
+    //Logout
     Route::post('/v1/logout', [UserController::class, 'logout']);
+
+    //Users
+    Route::prefix('v1/users')->group(function () {
+        Route::get('', [UserController::class, 'index'])
+            ->middleware('role:admin,junta'); 
+        Route::get('/{id}', [UserController::class, 'detail'])
+            ->middleware('role:admin,junta')
+            ->where('id', '[0-9]+'); 
+        Route::post('', [UserController::class, 'store'])
+            ->middleware('role:admin'); 
+        Route::put('/{id}', [UserController::class, 'update'])
+            ->where('id', '[0-9]+'); 
+        Route::delete('/{id}', [UserController::class, 'destroy'])
+            ->middleware('role:admin')    
+            ->where('id', '[0-9]+'); 
+    });
 });
